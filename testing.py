ac2360b59cbf7c5c5952851c914a6ff68450f1a6
Kevin McPherson <kpmcpherson@gmail.com>
2020-05-13 23:01:15 -0400

    writes unit test for assert

---
+++ testing.py
@@ -0,0 +1,6 @@
+''' Unit tests are tests that check a small component of the
+application. The following is a unit test for the method sum()'''
+
+assert sum([1,2,3]) == 6 'should be 6, won\'t print if correct'
+print ('Assertion is correction')
+
