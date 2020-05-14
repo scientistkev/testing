fc7a56dda20f5d337329f8bd87fb1954e2df9b5a
Kevin McPherson <kpmcpherson@gmail.com>
2020-05-13 23:11:35 -0400

    updates with assertion, entry point

--- test_sum.py
+++ test_sum.py
@@ -6,3 +6,13 @@ print ('Assertion is correction')
 
 assert sum([1,1,1]) == 6  'should be 6, will return AssertionError'
 
+''' A function of the assert sum() function, using name/main
+scripting ''' 
+
+def test_sum():
+    """ A function to test the sum method, in function form! """
+    assert sum([1,2,3]) == 6, 'should be 6, won\'t print if correct'
+
+if __name__ == "__main__": 'command line entry point'
+    test_sum() 'This is also called a test assertion'
+    print('Everything passed')
