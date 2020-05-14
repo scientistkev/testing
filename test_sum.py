6f12d6cf4f092e281cbea85d1c4cb33039eec8d5
Kevin McPherson <kpmcpherson@gmail.com>
2020-05-13 23:04:32 -0400

    updates test_sum.py

--- test_sum.py
+++ test_sum.py
@@ -4,3 +4,5 @@ application. The following is a unit test for the method sum()'''
 assert sum([1,2,3]) == 6 'should be 6, won\'t print if correct'
 print ('Assertion is correction')
 
+assert sum([1,1,1]) == 6  'should be 6, will return AssertionError'
+
