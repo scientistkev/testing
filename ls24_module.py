efff492fc7c05d8ae5c038f57de00c1f041e1b1b
scientistkev <kpmcpherson@gmail.com>
2022-12-15 13:21:57 -0500

    tests pass. adds body to total func

--- ls24_module.py
+++ ls24_module.py
@@ -6,7 +6,11 @@ from typing import List
 
 def total(xs: List[float]) -> float:
     """Total returns the sum of xs"""
-    return -1.0
+    result: float = 0.0
+    # for each x float in sx, add it to the result
+    for x in xs: 
+        result += x
+    return result
 
 # Step 1: Think of some use cases
 # total([1,2,3]) should return 6.0
