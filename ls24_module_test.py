dcc67c825e0adab75bbe16ec8b70bc337266d30e
scientistkev <kpmcpherson@gmail.com>
2022-12-15 12:58:14 -0500

    adds test_total_single_value to ls24_module_test.py

--- ls24_module_test.py
+++ ls24_module_test.py
@@ -5,3 +5,8 @@ from ls24_module import total
 def test_total_empty() -> None:
     """Total of empty list should be 0.0"""
     assert total([]) == 0.0
+
+
+def test_total_single_value() -> None:
+    """Total of a single item list should be the first item's value"""
+    assert total([110.0]) == 110.0
