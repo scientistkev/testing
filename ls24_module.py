b00137acf4c325b8d1c8e7ed1d5725170155cac9
scientistkev <kpmcpherson@gmail.com>
2022-12-15 12:39:38 -0500

    adds some context comments to ls24

--- ls24_module.py
+++ ls24_module.py
@@ -2,6 +2,14 @@
 
 from typing import List
 
+# Step 0: Implement skeleton function
+
 def total(xs: List[float]) -> float:
     """Total returns the sum of xs"""
     return -1.0
+
+# Step 1: Think of some use cases
+# total([1,2,3]) should return 6.0
+# total([110]) should return 110.0
+# total([]) should return 0.0
+
