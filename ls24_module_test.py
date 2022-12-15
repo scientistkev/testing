cb3f12fa34383465803e40d9cb1e37c9809322c4
scientistkev <kpmcpherson@gmail.com>
2022-12-15 13:25:17 -0500

    makes test_total_many_items and passes test

--- ls24_module_test.py
+++ ls24_module_test.py
@@ -10,3 +10,7 @@ def test_total_empty() -> None:
 def test_total_single_value() -> None:
     """Total of a single item list should be the first item's value"""
     assert total([110.0]) == 110.0
+
+def test_total_many_items() -> None:
+    """Total of a list with many items should be their sum."""
+    assert total([1.0,2.0,3.0]) == 6.0
